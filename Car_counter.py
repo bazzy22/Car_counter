6d8d2d4be2cd52e97843544d81d7c7c6fe749063
manuelAnd123 <manuel.andretto@studenti.unipd.it>
2025-09-28 12:12:23 +0200

    Car_counter using ultralytics computer vision

---
+++ Car_counter.py
@@ -0,0 +1,78 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import time
+from sort import *
+
+cap = cv2.VideoCapture("..\Project - car counter\Videos\cars.mp4")  # For Video
+
+
+model = YOLO("../Yolo-Weights/yolov8l.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+
+prev_frame_time = 0
+new_frame_time = 0
+
+mask = cv2.imread("..\Project - car counter\mask.png")
+
+#tracking with sort.py
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+
+while True:
+    new_frame_time = time.time()
+    success, img = cap.read()
+    imgRegion = cv2.bitwise_and(img, mask)
+
+    results = model(imgRegion, stream=True)
+
+    detections = np.empty((0,5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            w, h = x2 - x1, y2 - y1
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+
+            if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 0.4:
+
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
+                cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
+                currentArray = np.array([x1,y1,x2,y2,conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+
+    for results in resultsTracker:
+        x1, y1, x2, y2, Id = results
+        print(results)
+        w,h = x2-x1, y2-y1
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255,0,0))
+        cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
+
+    cv2.imshow("Image", img)
+    #cv2.imshow("ImageRegion", imgRegion)
+    cv2.waitKey(0)
+
+    # fps = 1 / (new_frame_time - prev_frame_time)
+    # prev_frame_time = new_frame_time
+    # print(fps)
