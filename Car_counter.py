11407ae59a28426642d8fb99454ee3cda9c8b028
manuelAnd123 <manuel.andretto@studenti.unipd.it>
2025-09-28 12:25:10 +0200

    added id tracker

--- Car_counter.py
+++ Car_counter.py
@@ -55,7 +55,7 @@ while True:
 
             if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 0.4:
 
-                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
+                #cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
                 cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
                 currentArray = np.array([x1,y1,x2,y2,conf])
                 detections = np.vstack((detections, currentArray))
@@ -63,11 +63,12 @@ while True:
     resultsTracker = tracker.update(detections)
 
     for results in resultsTracker:
-        x1, y1, x2, y2, Id = results
+        x1, y1, x2, y2, id = results
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         print(results)
         w,h = x2-x1, y2-y1
         cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255,0,0))
-        cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
+        cvzone.putTextRect(img, f' {id}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
 
     cv2.imshow("Image", img)
     #cv2.imshow("ImageRegion", imgRegion)
