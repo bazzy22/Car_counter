185da93ac7a9bf5ef37a71863fe5d477b4a920c6
manuelAnd123 <manuel.andretto@studenti.unipd.it>
2025-09-28 15:59:54 +0200

    added car counter based on car position

--- Car_counter.py
+++ Car_counter.py
@@ -8,7 +8,7 @@ from sort import *
 cap = cv2.VideoCapture("..\Project - car counter\Videos\cars.mp4")  # For Video
 
 
-model = YOLO("../Yolo-Weights/yolov8l.pt")
+model = YOLO("../Yolo-Weights/yolov8n.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -30,8 +30,11 @@ mask = cv2.imread("..\Project - car counter\mask.png")
 #tracking with sort.py
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
+limits = [380,297,673,297]
+totalCount = []
+
 while True:
-    new_frame_time = time.time()
+    #new_frame_time = time.time()
     success, img = cap.read()
     imgRegion = cv2.bitwise_and(img, mask)
 
@@ -56,24 +59,34 @@ while True:
             if currentClass == "car" or currentClass == "truck" or currentClass == "bus" or currentClass == "motorbike" and conf > 0.4:
 
                 #cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
-                cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
+                #cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=5)
                 currentArray = np.array([x1,y1,x2,y2,conf])
                 detections = np.vstack((detections, currentArray))
 
     resultsTracker = tracker.update(detections)
+    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
 
     for results in resultsTracker:
         x1, y1, x2, y2, id = results
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         print(results)
         w,h = x2-x1, y2-y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255,0,0))
-        cvzone.putTextRect(img, f' {id}', (max(0, x1), max(35, y1)), scale=0.6, thickness=1, offset=3)
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255,0,255))
+        cvzone.putTextRect(img, f' {int(id)}', (max(0, x1), max(35, y1)), scale=2, thickness=2, offset=7)
 
 
+        cx, cy = x1+w//2, y1+h//2
+        cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
+
+        if limits[0]<cx<limits[2] and limits[1]-20<cy<limits[1]+20:
+            if totalCount.count(id) ==0:
+                totalCount.append(id)
+                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 255), 5)
+
+    cvzone.putTextRect(img, f' Count: {len(totalCount)}', (100, 50))
     cv2.imshow("Image", img)
     #cv2.imshow("ImageRegion", imgRegion)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
 
     # fps = 1 / (new_frame_time - prev_frame_time)
     # prev_frame_time = new_frame_time
